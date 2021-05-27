59cb2b03c87406524fbbdb37ab52d64d8ff90d2e
mvsnogueira-dnx <marcus.nogueira@dnx.solutions>
2021-05-28 04:57:04 +1000

    Spot Instance

--- _variables.tf
+++ _variables.tf
@@ -142,3 +142,13 @@ variable "tags" {
   description = "Additional resource tags"
   type        = map(string)
 }
+
+variable "on_demand_base_capacity" {
+  description = "on_demand_base_capacity"
+  default     = ""
+}
+
+variable "on_demand_percentage" {
+  description = "on_demand_percentage"
+  default     = ""
+}
