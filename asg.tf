643d927244bdbbc1638403c013535cca9291831d
mvsnogueira-dnx <marcus.nogueira@dnx.solutions>
2021-05-31 21:52:10 +1000

    Including spot instance type

--- asg.tf
+++ asg.tf
@@ -5,10 +5,21 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = 1
   vpc_zone_identifier = list(var.instances_subnet_ids[count.index])
 
-  launch_template {
-    name    = aws_launch_template.default.name
-    version = "$Latest"
-  }
+   mixed_instances_policy { 
+
+    launch_template {
+      launch_template_specification {
+        launch_template_name = aws_launch_template.default.name
+        version = "$Latest"
+      }
+    }
+    instances_distribution {
+    
+      on_demand_base_capacity                  = var.on_demand_base_capacity
+      on_demand_percentage_above_base_capacity = var.on_demand_percentage
+    }
+
+}
 
   tags = concat(
     [ for key, value in var.tags: { key: key, value: value, propagate_at_launch: true } ],
